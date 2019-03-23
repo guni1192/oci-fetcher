4d9f292f719af86729b93bd0ebd4b31dc9973452
guni1192 <ad2314ce71926@gmail.com>
2019-03-23 07:30:09 +0000

    success download library/alpine

---
+++ src/image.rs
@@ -0,0 +1,139 @@
+use std::fs::{self, File};
+use std::io::{self, Error, ErrorKind, Write};
+use std::path::Path;
+
+use flate2::read::GzDecoder;
+use reqwest;
+use serde_json::{self, Value};
+use tar::Archive;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Image {
+    name: String,
+    tag: String,
+}
+
+impl Image {
+    pub fn new(name_and_tag: &str) -> Image {
+        let mut n: Vec<&str> = name_and_tag.split(':').collect();
+        if n.len() < 2 {
+            n.push("latest");
+        }
+        Image {
+            name: n[0].to_string(),
+            tag: n[1].to_string(),
+        }
+    }
+
+    pub fn build_from_tar(&self, path: &str, container_id: &str) -> io::Result<()> {
+        let tar_gz = File::open(&path).expect("");
+        let tar = GzDecoder::new(tar_gz);
+        let mut ar = Archive::new(tar);
+
+        let image_path = container_id;
+
+        if !Path::new(&image_path).exists() {
+            std::fs::create_dir_all(&image_path)?;
+        }
+
+        for file in ar.entries().unwrap() {
+            let mut file = file.unwrap();
+            if file.unpack_in(&image_path).is_ok() {
+                continue;
+            };
+        }
+
+        Ok(())
+    }
+
+    pub fn put_config_json(&self, manifests: String, container_id: &str) -> std::io::Result<()> {
+        let manifests = manifests.as_bytes();
+
+        let config_path = format!("{}/cromwell", container_id);
+        fs::create_dir_all(&config_path).expect("Cannot create CONTAINER_PATH/cromwell/");
+
+        let mut file = File::create(config_path + "/manifests.json")?;
+        file.write_all(manifests)?;
+        // TODO: parse manifests.history.v1Compatibility
+
+        Ok(())
+    }
+
+    pub fn pull(&mut self, container_id: &str) -> Result<(), reqwest::Error> {
+        let auth_url = format!(
+            "https://auth.docker.io/token?service=registry.docker.io&scope=repository:{}:pull",
+            self.name
+        );
+        let res_json: String = reqwest::get(auth_url.as_str())?.text()?;
+        let body: Value = serde_json::from_str(res_json.as_str()).expect("parse json failed");
+
+        let token = match &body["token"] {
+            Value::String(t) => t,
+            _ => panic!("unexpected data: body[\"token\"]"),
+        };
+
+        let manifests_url = format!(
+            "https://registry.hub.docker.com/v2/{}/manifests/{}",
+            self.name, self.tag
+        );
+
+        let res = reqwest::Client::new()
+            .get(manifests_url.as_str())
+            .bearer_auth(token)
+            .send()?
+            .text()?;
+
+        let body: Value = serde_json::from_str(res.as_str()).expect("parse json failed");
+
+        let manifests = serde_json::to_string(&body).expect("Cannot convert Value to string");
+
+        match &body["fsLayers"] {
+            Value::Array(fs_layers) => {
+                for fs_layer in fs_layers {
+                    self.download(token, &fs_layer, container_id)
+                        .expect("download failed");
+                }
+            }
+            _ => eprintln!("unexpected type fsLayers"),
+        }
+
+        self.put_config_json(manifests, container_id)
+            .expect("cannnot put jsno");
+
+        Ok(())
+    }
+
+    fn download(&self, token: &str, fs_layer: &Value, container_id: &str) -> std::io::Result<()> {
+        if let Value::String(blob_sum) = &fs_layer["blobSum"] {
+            let out_filename = format!("/tmp/{}.tar.gz", blob_sum.replace("sha256:", ""));
+
+            if Path::new(out_filename.as_str()).exists() {
+                self.build_from_tar(&out_filename, container_id)
+                    .expect("cannnot build from tar");
+                return Ok(());
+            }
+
+            let url = format!(
+                "https://registry.hub.docker.com/v2/{}/blobs/{}",
+                self.name, blob_sum
+            );
+
+            let mut res = reqwest::Client::new()
+                .get(url.as_str())
+                .bearer_auth(token)
+                .send()
+                .expect("failed to send requwest");
+            let mut out = File::create(&out_filename)?;
+
+            io::copy(&mut res, &mut out)?;
+            self.build_from_tar(&out_filename, container_id)
+                .expect("cannnot build from tar");
+        } else {
+            return Err(Error::new(
+                ErrorKind::Other,
+                "blobSum not found from fsLayer",
+            ));
+        }
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
+#[macro_use]
+extern crate serde_derive;
+
 use std::process;
 
-use clap::{crate_name, crate_version, crate_authors, App, Arg, SubCommand, ArgMatches};
+use clap::{crate_authors, crate_name, crate_version, App, Arg, ArgMatches, SubCommand};
 
-fn pull(sub_m: &ArgMatches) {
+mod image;
 
+use self::image::Image;
+
+fn do_pull(sub_m: &ArgMatches) {
+    let mut image = Image::new(sub_m.value_of("image_name").expect("specify image name"));
+    let path = sub_m.value_of("dir_name").unwrap_or("./hoge");
+    image.pull(path).expect("failed fetch oci image");
 }
 
 fn main() {
@@ -31,13 +40,14 @@ fn main() {
                         .short("o")
                         .value_name("NAME")
                         .help("Specify put dir name")
+                        .required(true)
                         .takes_value(true),
                 ),
         )
         .get_matches();
 
-     match &app_matches.subcommand() {
-        ("pull", Some(sub_m)) => pull(&sub_m),
+    match &app_matches.subcommand() {
+        ("pull", Some(sub_m)) => do_pull(&sub_m),
         _ => {
             eprintln!("Unexpected arguments");
             app.print_help().unwrap();
@@ -45,5 +55,4 @@ fn main() {
             process::exit(1);
         }
     }
-
 }
